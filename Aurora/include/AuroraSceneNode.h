9c05f237b83a2d9d17b8d84de607e0b613645a9b
David Davidović <geosoft.corp@gmail.com>
2011-08-11 00:13:02 +0200

    wrote SceneNode header

--- Aurora/include/AuroraSceneNode.h
+++ Aurora/include/AuroraSceneNode.h
@@ -1,18 +1,18 @@
 /*
-    This file is part of Aurora Game Engine.
+	This file is part of Aurora Game Engine.
 
-    Aurora Game Engine is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	Aurora Game Engine is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    Aurora Game Engine is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
+	Aurora Game Engine is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public License
-    along with Aurora Game Engine.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU Lesser General Public License
+	along with Aurora Game Engine.  If not, see <http://www.gnu.org/licenses/>.
 
 	Copyright (C) David Davidovic (Geomaster) 2011.
 
@@ -34,22 +34,22 @@ namespace Aurora
 	public:
 		SceneNode(String Name, Scene* Owner, Node* Parent = NULL) : Node(Name, Parent), mScene(Owner) { ; }
 
-        virtual Node* createChild(String Name, const Transform& ChildTransform = Transform())
-        {
-        	return createChildSceneNode(Name, ChildTransform);
-        }
+		virtual Node* createChild(String Name, const Transform& ChildTransform = Transform())
+		{
+			return createChildSceneNode(Name, ChildTransform);
+		}
 
-        virtual Node* createChild(String Name, const Vector3D& Translation = Vector3D::Zero,
-                         const Quaternion& Rotation = Quaternion::Identity,
-                         const Vector3D& Scale = Vector3D(Real( 1.0 )))
+		virtual Node* createChild(String Name, const Vector3D& Translation = Vector3D::Zero,
+						 const Quaternion& Rotation = Quaternion::Identity,
+						 const Vector3D& Scale = Vector3D(Real( 1.0 )))
 		{
 			return createChildSceneNode(Name, Translation, Rotation, Scale);
 		}
 
 		virtual SceneNode* createChildSceneNode(String Name, const Transform& ChildTransform = Transform());
 		virtual SceneNode* createChildSceneNode(String Name, const Vector3D& Translation = Vector3D::Zero,
-                         const Quaternion& Rotation = Quaternion::Identity,
-                         const Vector3D& Scale = Vector3D(Real( 1.0 )));
+						 const Quaternion& Rotation = Quaternion::Identity,
+						 const Vector3D& Scale = Vector3D(Real( 1.0 )));
 
 		virtual void attachEntity(Entity* NewEntity);
 		virtual void detachEntity(Entity* OldEntity);
